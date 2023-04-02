b0275a8348c62799172ca74bc19a242ec5ecdf16
Iraiore <yagoiraiore@gmail.com>
2023-04-02 16:41:21 -0400

    instalando os pacotes e configurando os arquivos tsconfig.json, main.ts e package.json

---
+++ .history/src/main_20230402163739.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', function(request, response){
+    response.send('API contatos rodando...')
+});
+
+a
---
+++ .history/src/main_20230402163809.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', function(request, response){
+    response.send('API contatos rodando...')
+});
+
+app.listen(port, function(){
+    res
+});
