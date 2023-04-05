98cbfa5a50c72eaf8eadadd2e6a6d959d806ef94
Pedro Yago Iraiore Carvalho <2021202060026@aluno.ifro.edu.br>
2023-04-05 18:44:00 -0400

    corrigindo o erro do incrmento e salvando o arquivo

--- src/router.ts
+++ src/router.ts
@@ -4,12 +4,10 @@ import fs from 'fs';
 const router = Router();
 const ARQUIVO = `${__dirname}/storage/contatos.json`;
 
-
-let id = 0;
 let contatos = Array(); //array para salvar novos contatos
 
 //Rota principal (/), retornando um arquivo em formato json
-router.get('/', function(request, response) {
+router.get('/', function (request, response) {
     response.send({
         api_name: 'api-contatos',
         descricao: 'API para gestão de contatos',
@@ -18,38 +16,37 @@ router.get('/', function(request, response) {
 });
 
 //rota com as informnações sobre o autor
-router.get('/sobre',function(request, response) {
+router.get('/sobre', function (request, response) {
     response.send({
-        nome:'Pedro Yago',
-        email:'yagoiraiore@gmail.com',
-        GitHub: 'github.com/iraiore' 
+        nome: 'Pedro Yago',
+        email: 'yagoiraiore@gmail.com',
+        GitHub: 'github.com/iraiore'
     });
 });
 
 
 //retornando os dados do request.body no response da rora /contato
-router.post('/contato', function(request, response){
+router.post('/contato', function (request, response) {
     let ok = false;
     let mensagem = "";
 
     if (request.body.nome == undefined || request.body.nome == "") {
-        if(request.body.nome == "") {
-         mensagem = "A propriedade [nome] não deve estar em branco!";
+        if (request.body.nome == "") {
+            mensagem = "A propriedade [nome] não deve estar em branco!";
         }
-     } else {
-         mensagem = "O contato [" + request.body.nome + "] foi salvo com sucesso!";
-         ok = true;
-     }
-     if(ok){
-        id++;
+    } else {
+        mensagem = "O contato [" + request.body.nome + "] foi salvo com sucesso!";
+        ok = true;
+    }
 
-        //loadContatos();
+    if (ok) {
+        loadContatos();
         //adiciona o contato na variavel
-        contatos.push({ id, ...request.body });
+        contatos.push({ id: contatos.length + 1, ...request.body });
 
         //Escreve no arquivo, salva todos os contatos no arquivo
         fs.writeFileSync(ARQUIVO, JSON.stringify(contatos));
-     }
+    }
 
     response.send({
         sucess: ok,
@@ -57,12 +54,12 @@ router.post('/contato', function(request, response){
     });
 
     //rota para receber os dados do novo contato
-    router.post('/contato', function(request, response){
+    router.post('/contato', function (request, response) {
         console.log(request.body);
     });
 });
 
-router.get('/contato',function(request, response){
+router.get('/contato', function (request, response) {
     //carrega dados do arquivo
     loadContatos();
 
@@ -75,12 +72,12 @@ function loadContatos() {
     //verifica se o arquivo já existe, caso não exista ele cria 
     if (!fs.existsSync(ARQUIVO))
         fs.writeFileSync(ARQUIVO, JSON.stringify([]));
-        
-     //lê o arquivo e adiciona o conteúdo na variável data
-     const data = fs.readFileSync(ARQUIVO);
-     
-     //converte o conteúdo do arquivo para JSON
-     contatos = JSON.parse(data.toString());
+
+    //lê o arquivo e adiciona o conteúdo na variável data
+    const data = fs.readFileSync(ARQUIVO);
+
+    //converte o conteúdo do arquivo para JSON
+    contatos = JSON.parse(data.toString());
 }
 
 export default router;
